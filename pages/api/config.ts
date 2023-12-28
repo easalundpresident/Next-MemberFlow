644c9fa3e01777339833712ae3a14010a8a99d5d
easalundpresident <151455539+easalundpresident@users.noreply.github.com>
2023-12-28 14:49:05 +0000

    fix: update firebase config to use EASA

--- pages/api/config.ts
+++ pages/api/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDTGhSkF30MwxHi8ug0_a-JoeVg2ARLkls",
-  authDomain: "memberflow-api.firebaseapp.com",
-  projectId: "memberflow-api",
-  storageBucket: "memberflow-api.appspot.com",
-  messagingSenderId: "207860370287",
-  appId: "1:207860370287:web:ec9a05922115db8276769a",
-  measurementId: "G-JPKKS1672S",
-};
+  apiKey: "AIzaSyBxR8B20qOCENQNLjxeM2PDHogZJFJXvdA",
+  authDomain: "easa-477a3.firebaseapp.com",
+  projectId: "easa-477a3",
+  storageBucket: "easa-477a3.appspot.com",
+  messagingSenderId: "477516979855",
+  appId: "1:477516979855:web:dfdfdcbd995fbe136c1afa",
+  measurementId: "G-H1LBH06J56"
+}
